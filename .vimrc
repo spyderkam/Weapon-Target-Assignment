01e6125123448fb230efdb6f98728243ef7791e2
Kam <kamyarmojtahed@gmail.com>
2025-09-23 18:43:48 +0000

    Added `alpha` w/o proper `f(rho)`.
    Just using regular `rho` right now. Also, moved `.vimrc` to from `~/workspace` to `~` directory, so it won't show on GitHub.

--- .vimrc
+++
@@ -1,8 +0,0 @@
-" Display existing tab characters as 4 spaces
-set tabstop=4
-" Use 4 spaces for each step of (auto)indent
-set shiftwidth=4
-" When the <Tab> key is pressed, insert spaces instead of a tab character
-set expandtab
-" Ensure syntax highlighting remains enabled
-syntax on
--- src/wta_sim.adb
+++ src/wta_sim.adb
@@ -6,7 +6,7 @@ procedure weapon_target_assignment is
     package Float_Elementary_Functions is new Ada.Numerics.Generic_Elementary_Functions(Float);
 
     use Ada.Strings;
-    use Ada.Text_IO;
+    use Ada.Text_IO;  -- Include this line to use Put_Line directly
     use Float_Elementary_Functions;
 
     type Weapon is record
@@ -36,14 +36,24 @@ procedure weapon_target_assignment is
         return Distance(w, t) <= w.R_max;
     end In_Range;
 
+    -- $\alpha_{\imath}{\jmath}$
+    function Vulnerability_Coefficient(w: Weapon; t: Target) return Float is
+    begin
+     	return (w.beta/(t.A*t.S)) * t.rho;  -- Eventually need to replace rho with f(rho).
+    end Vulnerability_Coefficient;
+
 begin
     declare
-        weap1 : Weapon := (x => 0.0, y => 0.0, z => 0.0, beta => 1.0, R_max => 100.0, C_available => 10);
-        targ1 : Target := (x => 50.0, y => 30.0, z => 0.0, vx => 0.0, vy => 0.0, vz => 0.0, A => 2.0, S => 1.5, rho => 0.8);
-        dist  : Float;
+        weap1   : Weapon := (x => 0.0, y => 0.0, z => 0.0, beta => 1.0, R_max => 100.0, C_available => 10);
+        targ1   : Target := (x => 50.0, y => 30.0, z => 0.0, vx => 0.0, vy => 0.0, vz => 0.0, A => 2.0, S => 1.5, rho => 0.8);
+        dist    : Float;
+        alpha11 : Float;
     begin
         dist := Distance(weap1, targ1);
-        Put_Line(Fixed.Trim("Weapon 1 to Target 1 distance: " & Float'Image(dist) & " m", Left));
+        alpha11 := Vulnerability_Coefficient(weap1, targ1);
+        Put_Line(Fixed.Trim("Weapon 1 to Target 1 distance:" & Float'Image(dist) & " m", Left));
         Put_Line(Fixed.Trim("Target in range: " & Boolean'Image(In_Range(weap1, targ1)), Left));
+        Put_Line("Vulnerability Coefficient:" & Float'Image(alpha11));
     end;
 end weapon_target_assignment;
+
